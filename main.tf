eaf17fd49cb8a099e6b41426ba820ceb84d6c545
David Gevargizian <david.gevargizian@hrblock.com>
2019-10-20 16:50:03 -0500

    t

--- main.tf
+++ main.tf
@@ -12,12 +12,31 @@ module "ec2_cluster" {
   ami                    = "ami-ebd02392"
   instance_type          = "t2.micro"
   key_name               = "user1"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
-  subnet_id              = "subnet-eddcdzz4"
+  monitoring             = false
+  #vpc_security_group_ids = ["sg-12345678"]
+  #subnet_id              = "subnet-eddcdzz4"
 
   tags = {
     Terraform   = "true"
     Environment = "dev"
-  }
+  } 
 }
+
+# module "vpc" {
+#   source = "terraform-aws-modules/vpc/aws"
+
+#   name = "my-vpc"
+#   cidr = "10.0.0.0/16"
+
+#   azs             = ["us-west-2"]
+#   private_subnets = ["10.0.1.0/24"]
+#   public_subnets  = ["10.0.101.0/24"]
+
+#   enable_nat_gateway = true
+#   enable_vpn_gateway = true
+
+#   tags = {
+#     Terraform = "true"
+#     Environment = "dev"
+#   }
+# }
